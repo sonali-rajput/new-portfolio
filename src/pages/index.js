db27beff55bac305f9e5dbced3e2b663a91449c8
sonali-rajput <sonalirajput1088@gmail.com>
2025-05-29 12:36:03 +0100

    homepage for sonali

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,128 +1,86 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+import Layout from '../components/layout';
+import ProfilePicture from '../components/ProfilePicture';
+import {
+  IndexLinkBlock,
+  IndexLinkBlockSet,
+} from '../components/IndexLinkBlock';
+import IndexHeader from '../components/IndexHeader';
+import './index.css';
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+const styles = {
+  title: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row-reverse',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 40,
+    flexWrap: 'wrap',
   },
-  {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-    description:
-      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
-]
+  headline: { paddingLeft: '1.25rem', textAlign: 'center' },
+  tagLine: { fontSize: 18, paddingBottom: 14 },
+};
 
-const samplePageLinks = [
-  {
-    text: "Page 2",
-    url: "page-2",
-    badge: false,
-    description:
-      "A simple example of linking to another page within a Gatsby site",
-  },
-  { text: "TypeScript", url: "using-typescript" },
-  { text: "Server Side Rendering", url: "using-ssr" },
-  { text: "Deferred Static Generation", url: "using-dsg" },
-]
+const TagLine = () => (
+<div style={styles.tagLine}>
+  I&apos;m a Software Engineer - DevOps, Platform, AI who loves creating and experimenting with technology. 
+  When I&apos;m not building cool stuff, you&apos;ll find me learning new skills 
+  and sharing knowledge with fellow developers.
+</div>
+);
 
-const moreLinks = [
-  { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
-  {
-    text: "Documentation",
-    url: "https://gatsbyjs.com/docs/",
-  },
-  {
-    text: "Starters",
-    url: "https://gatsbyjs.com/starters/",
-  },
-  {
-    text: "Showcase",
-    url: "https://gatsbyjs.com/showcase/",
-  },
-  {
-    text: "Contributing",
-    url: "https://www.gatsbyjs.com/contributing/",
-  },
-  { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
-]
+const IndexPage = () => {
+  return (
+    <>
+    <IndexHeader />
+    <Layout
+      showHeader={false}
+      description="The homepage of Sonali Rajput / Heyiamsra"
+      >
+        <div style={styles.title}>
+          <h1 style={styles.headline}>Sonali Rajput // Heyiamsra</h1>
+          <ProfilePicture />
+        </div>
 
-const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
+        <TagLine />
 
-const IndexPage = () => (
-  <Layout>
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
-    </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
-  </Layout>
-)
+        <IndexLinkBlockSet>
+          <IndexLinkBlock to="/portfolio/" text="Projects + Work Portfolio" />
+          <IndexLinkBlock to="/blog/" text="Blog" />
+          <IndexLinkBlock to="/contact/" text="Contact" />
+          <IndexLinkBlock to="/professional" text="Professional Skills and Experience" />
+        </IndexLinkBlockSet>
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="Home" />
+        style={{ display: 'none' }}
+        rel="me"
+        href="https://mastodon.social/@heyiamsra"
+      </Layout>
+    </>
+  )
+}
 
-export default IndexPage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
